8309e92c6fcfdb713508883369bc43670a24a807
Mohammad Rezaii <rezaiimohammad003@gmail.com>
2024-04-02 20:33:50 +0330

    [NEW] adding the create invoice and webhook endpoint

--- backend/controllers/paymentControllers.js
+++ backend/controllers/paymentControllers.js
@@ -1,10 +1,59 @@
 import asyncHandler from "express-async-handler";
+import crypto from "crypto";
+import axios from "axios";
+import Order from "../models/orderModel.js";
 
 // @desc    Creates an invoice
 // @api     POST /api/payment/create-invoice
 // @access  Private
 const createInvoice = asyncHandler(async (req, res) => {
-  res.send("Hello World!");
+  const { totalAmount, orderId } = req.body;
+
+  const invoiceData = {
+    amount: totalAmount,
+    currency: "USD",
+    order_id: orderId,
+    url_success: `https://frontend-mern-ecommerce-oihi.onrender.com/order/${orderId}`,
+    url_callback:
+      "https://backend-mern-ecommerce-86ui.onrender.com/api/payment/status-callback",
+    lifetime: 300,
+  };
+
+  const authData = {
+    merchant: process.env.CRYPTOMUS_MERCHANT_UUID,
+    sign: crypto
+      .createHash("md5")
+      .update(
+        Buffer.from(JSON.stringify(invoiceData)).toString("base64") +
+          process.env.CRYPTOMUS_PAYMENT_API_KEY
+      )
+      .digest("hex"),
+  };
+
+  const { data } = await axios.post(
+    "https://api.cryptomus.com/v1/payment",
+    invoiceData,
+    {
+      headers: {
+        ...authData,
+      },
+    }
+  );
+
+  const order = await Order.findById(orderId);
+  order.paymentResult = {
+    id: data.result.uuid,
+    status: data.result.payment_status,
+  };
+  order.save();
+
+  res.json(data);
+});
+
+const statusCallback = asyncHandler(async (req, res) => {
+  const paymentStatus = req.body;
+
+  console.log(paymentStatus);
 });
 
 export { createInvoice };
--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -4,5 +4,6 @@ import { createInvoice } from "../controllers/paymentControllers.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.post("/create-invoice", protect, createInvoice);
+router.post("/status-callback", protect, createInvoice);
 
 export default router;
