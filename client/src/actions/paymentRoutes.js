b3afcab2ea6e52d166db006a13109132f9b69155
Yash <yashvarmora8484@gmail.com>
2024-09-29 16:54:55 +0530

    changes saved

---
+++ client/src/actions/paymentRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+const router = express.Router();
+import {
+  createInvoice,
+  statusCallback,
+} from "../controllers/paymentControllers.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+router.post("/create-invoice", protect, createInvoice);
+router.post("/status-callback", statusCallback);
+
+export default router;
